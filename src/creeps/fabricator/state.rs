114e917727df35d4cbad1de4ba72bf942fe24aa0
Alexander Wahlsten <alexander.wahlsten@gmail.com>
2026-07-07 23:29:38 +0200

    Made fabricators idle close buffer

--- src/creeps/fabricator/state.rs
+++ src/creeps/fabricator/state.rs
@@ -41,6 +41,10 @@ impl FabricatorCreep {
                     next!(Self::Performing(task))
                 }
 
+                if let Some(buffer) = &home.buffer {
+                    defer!(movement.move_vcreep_to(creep, buffer.pos(), 5), self)?;
+                }
+
                 Ok(Done(self))
             },
             Self::CollectingFor(ref task) => {
